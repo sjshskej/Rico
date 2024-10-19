e346c913e9077c3fcd24e01f2cb9db0b2e5ed7db
sjshskej <narwtwrby@gmail.com>
2024-10-19 13:00:31 +0300

    Ricopo.lua

--- Ricopo.lua
+++ Ricopo.lua
@@ -1,38 +1,45 @@
-repeat task.wait(0.25) until game:IsLoaded();
-getgenv().Image = "rbxassetid://7229442422"; -- put a asset id in here to make it work
-getgenv().ToggleUI = "E" -- This where you can Toggle the Fluent ui library
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Opboltejshshskidhdbd/Main/main/two_party_gamer-obfuscated%20(1).txt"))()
 
-task.spawn(function()
-    if not getgenv().LoadedMobileUI == true then getgenv().LoadedMobileUI = true
-        local OpenUI = Instance.new("ScreenGui");
-        local ImageButton = Instance.new("ImageButton");
-        local UICorner = Instance.new("UICorner");
-        OpenUI.Name = "OpenUI";
-        OpenUI.Parent = game:GetService("CoreGui");
-        OpenUI.ZIndexBehavior = Enum.ZIndexBehavior.Sibling;
-        ImageButton.Parent = OpenUI;
-        ImageButton.BackgroundColor3 = Color3.fromRGB(105,105,105);
-        ImageButton.BackgroundTransparency = 0.8
-        ImageButton.Position = UDim2.new(0.9,0,0.1,0);
-        ImageButton.Size = UDim2.new(0,50,0,50);
-        ImageButton.Image = getgenv().Image;
-        ImageButton.Draggable = true;
-        ImageButton.Transparency = 1;
-        UICorner.CornerRadius = UDim.new(0,200);
-        UICorner.Parent = ImageButton;
-        ImageButton.MouseButton1Click:Connect(function()
-            game:GetService("VirtualInputManager"):SendKeyEvent(true,getgenv().ToggleUI,false,game);
-        end)
-    end
+local Main = _G.Main:New("Main")
+local Tab1 = Main:Tab("Tab1")
+
+local Button1 = Tab1:Button("Button1", function()
+    print("Button1 clicked!")
 end)
-local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
 
-local Window = Fluent:CreateWindow({
-    Title = "Fluent " .. Fluent.Version,
-    SubTitle = "by dawid",
-    TabWidth = 160,
-    Size = UDim2.fromOffset(580, 460),
-    Acrylic = true, -- The blur may be detectable, setting this to false disables blur entirely
-    Theme = "Dark",
-    MinimizeKey = Enum.KeyCode.E -- Used when theres no MinimizeKeybind
+local Toggle1 = Tab1:Toggle({
+  Text = "Toggle1 (This prints toggle state)",
+  Callback = function(a)
+    print(a)
+  end
+  })
+
+local toggle2 = Tab1:Toggle({
+    Text = "ToggleName (more text = full animation)",
+    Callback = function()
+  print("hi")
+    end
 })
+
+local Label1 = Tab1:Label("Never gonna give you up")
+local Label2 = Tab1:Label("Never gonna let you down")
+local Label3 = Tab1:Label("Never gonna run around and desert you")
+local Label4 = Tab1:Label("Never gonna make you cry")
+local Label5 = Tab1:Label("Never gonna say goodbye")
+local Label6 = Tab1:Label("Never gonna tell a lie and hurt you")
+
+local Tab2 = Main:Tab("Tab2")
+
+local Button2 = Tab2:Button("Button2", function()
+    print("Button2 clicked!")
+end)
+local Toggle3 = Tab2:Toggle({
+    Text = "Toggle3",
+    Callback = function()
+    print("Boooooom!!!!!!!!!")
+end
+})
+
+local Label7 = Tab2:Label("How you feeling after getting rick rolled?")
+
+return _G.Main
