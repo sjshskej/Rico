a23f33aa5505d8f7bd35008c273a7a9b8419bd01
sjshskej <narwtwrby@gmail.com>
2024-11-22 15:58:24 +0300

    hdih.lua

--- hdih.lua
+++ hdih.lua
@@ -196,10 +196,7 @@ local function translateTextAsync(originalText)
         ["Voting Booth"] = "كشك التصويت", 
         ["Mayor Candidate"] = "مرشح لرئاسة البلدية", 
         ["Vote new mayor"] = "صوّت لعمدة جديد", 
-        ["Courtroom"] = "قاعة المحكمة", 
-        ["Witness Protection Program"] = "برنامج حماية الشهود", 
-        ["Mayor Office"] = "مكتب العمدة", 
-        ["Brookhaven Mayor"] = "عمدة بروكهافن"
+        ["Courtroom"] = "قاعة المحكمة"
     }
 
     return translations[originalText] or originalText -- إذا لم تكن الترجمة موجودة، يبقى النص كما هو
