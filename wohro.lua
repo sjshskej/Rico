96e63c2c4b37c4abca251f3e31e240d6a2db9b02
sjshskej <narwtwrby@gmail.com>
2024-11-29 05:22:41 +0300

    wohro.lua

--- wohro.lua
+++ wohro.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL3Nqc2hza2VqL1JpY28vcmVmcy9oZWFkcy9tYWluL3dvaHJvLmx1YQ==", true))()
+loadstring(game:HttpGet("aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL3Nqc2hza2VqL1JpY28vcmVmcy9oZWFkcy9tYWluL2hkaWgubHVh", true))()
