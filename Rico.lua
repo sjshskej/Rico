ee7d405fa88fa6cdb60b003dd5a1e6b0bb2ab649
sjshskej <narwtwrby@gmail.com>
2024-12-27 19:03:03 +0300

    Rico.lua

--- Rico.lua
+++ Rico.lua
@@ -40,6 +40,54 @@ local MainTab = Window:Tab({
 
 Window:SelectTab(1)
 
+local EmptyTab = Window:Tab({
+    Title = "Empty Tab",
+    Icon = "frown",
+})
+
+local EmptyTab2 = Window:Tab({
+    Title = "Tab Without icon",
+})
+
+local NotificationTab = Window:Tab({
+    Title = "Notification Tab",
+    Icon = "bell",
+})
+
+local BlockedElementsTab = Window:Tab({
+    Title = "Blocked Elements",
+    Icon = "rbxassetid://120011858138977",
+})
+
+local Divider = Window:Divider()
+
+local TabWithNewIcon = Window:Tab({
+    Title = "Tab with new Icon",
+    Icon = "book-user",
+})
+
+local Divider = Window:Divider()
+
+local WindowTab = Window:Tab({
+    Title = "Window and File Configuration",
+    Icon = "settings",
+})
+local CreateThemeTab = Window:Tab({
+    Title = "Create theme",
+    Icon = "palette",
+})
+
+local Divider = Window:Divider()
+
+for i=1, 20 do
+    Window:Tab({
+        Title = "Just An Empty Tab",
+        Image = "grid"
+    })
+end
+
+-- Main Tab
+
 MainTab:Section({ 
     Title = "Big section!",
     TextSize = 22,
