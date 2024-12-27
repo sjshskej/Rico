e1d6a778fe74450dc72d3b37f69d74a53024652c
sjshskej <narwtwrby@gmail.com>
2024-12-27 23:04:16 +0300

    Rico.lua

--- Rico.lua
+++ Rico.lua
@@ -86,37 +86,6 @@ local Code = Tab:Code({
 ]]
 })
 
-MainTab:Section({ Title = "مهم❓" })
-
-local Dialog = Window:Dialog({
-    Title = "الحوار",
-    Content = "تواصل معاي فل تليجرام @F_HPB",
-    Buttons = {
-        {
-            Title = "نعم",
-            Callback = function()
-                print("confirm")
-            end
-        },
-        {
-            Title = "لا",
-            Callback = function()
-                print("cancel")
-            end
-        },
-        {
-            Title = "الغاء",
-            Callback = function()
-                print("idk")
-            end
-        }
-    }
-})
-
-local Button = MainTab:Button({
-    Title = "حوار",
-    Desc = "ذا كانت عندك مشاكل فا اضغط هنا",
-    Callback = function()
-        Dialog:Open()
-    end,
-})
+local NotificationTab = Window:Tab({
+    Title = "المشيات",
+    Icon = "bell",
