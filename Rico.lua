e0093460dd9e14af539a195c6d0d60985bc2c18d
sjshskej <narwtwrby@gmail.com>
2024-12-28 19:03:48 +0300

    Rico.lua

--- Rico.lua
+++ Rico.lua
@@ -1,41 +1,93 @@
---[[
-Credits goes to REDz.
-And you can find more here! [This is v5 version]
-https://github.com/REDzHUB/RedzLibV5
-And maybe some things may not work correctly (doesn't to me)
-]]--
+local success, WindUI = pcall(function()
+    return loadstring(game:HttpGet("https://tree-hub.vercel.app/api/UI/WindUI"))()
+end)
 
-local redzlib = loadstring(game:HttpGet("https://raw.githubusercontent.com/REDzHUB/RedzLibV5/main/Source.Lua"))()
+if not success then
+    warn("Failed to load WindUI. Please check the URL or your internet connection.")
+    return
+end
 
-local Window = redzlib:MakeWindow({
-  Title = "Test",
-  SubTitle = "hello",
-  SaveFolder = "Redz Config"
+-- إنشاء نافذة
+local Window = WindUI:CreateWindow({
+    Title = "قائمة العرب",
+    Icon = "door-open",
+    Author = ".ftgs",
+    Folder = "CloudHub",
+    Size = UDim2.fromOffset(580, 460),
+    KeySystem = {
+        Key = "mkl",
+        Note = "رمز mkl",
+        URL = "https://github.com/Footagesus/WindUI",
+        SaveKey = true,
+    },
+    Transparent = true,
+    Theme = "Dark",
+    SideBarWidth = 200,
+    HasOutline = false,
 })
 
---Tabs
-
-local Tab1 = Window:MakeTab({"Tab 1", "cool"})
-
---Buttons
-
-Tab1:AddButton({"Print", function()
-print("Hello World!")
-end})
-
-local Toggle1 = Tab1:AddToggle({
-  Name = "Speed",
-  Description = "Idk",
-  Default = false
+-- زر فتح النافذة
+Window:EditOpenButton({
+    Title = "اضغط لفتح",
+    Icon = "image-upscale",
+    CornerRadius = UDim.new(0, 10),
+    StrokeThickness = 3,
+    Color = ColorSequence.new(
+        Color3.fromHex("FF0F7B"),
+        Color3.fromHex("F89B29")
+    ),
 })
 
-Tab1:AddSlider({
-  Name = "Speed",
-  Min = 1,
-  Max = 100,
-  Increase = 1,
-  Default = 16,
-  Callback = function(Value)
-  game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = Value
-  end
+-- إنشاء التبويب الأول
+local Tab1 = Window:Tab({
+    Title = "البداية",
+    Icon = "house",
 })
+
+local Section1 = Tab1:Section({
+    Title = "قائمة العرب",
+    TextXAlignment = "Left",
+    TextSize = 17,
+})
+
+Tab1:Paragraph({
+    Title = "متى تم انشاء هذا سكربت",
+    Desc = "2024 - الجمعة، 26 ديسمبر",
+})
+
+Tab1:Code({
+    Title = "main.lua",
+    Locked = false,
+    Code = [[
+-- مقدمة سكربت لتحسين تجربة المستخدم في اللعبة.
+-- يحتوي على أدوات تخصيص متكاملة.
+    ]],
+})
+
+-- إنشاء التبويب الثاني
+local Tab2 = Window:Tab({
+    Title = "المشيات",
+    Icon = "folder",
+})
+
+local Section2 = Tab2:Section({
+    Title = "المشيات",
+    TextXAlignment = "Left",
+    TextSize = 17,
+})
+
+Tab2:Paragraph({
+    Title = "آخر تحديثات",
+    Desc = "2024 - الجمعة، 27 ديسمبر",
+})
+
+Tab2:Button({
+    Title = "تحميل سكربت",
+    Desc = "اضغط لتحميل السكربت",
+    Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/sjshskej/Rico/refs/heads/main/ksokw.lua", true))()
+    end,
+})
+
+-- تحديد التبويب الافتراضي
+Window:SelectTab(1)
