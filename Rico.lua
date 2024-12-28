20008e500b74957fefd35c0aba589535c707538e
sjshskej <narwtwrby@gmail.com>
2024-12-28 19:51:31 +0300

    Rico.lua

--- Rico.lua
+++ Rico.lua
@@ -102,3 +102,24 @@ Animate.fall.FallAnim.AnimationId = "http://www.roblox.com/asset/?id=891617961"
 game.Players.LocalPlayer.Character.Humanoid.Jump = true
     end,
 })
+
+Tab2:Button({
+    Title = "المشية الثانية",
+    Desc = "الفوارة",
+    Callback = function()
+        local Animate = game.Players.LocalPlayer.Character:WaitForChild("Animate")
+
+-- إعدادات حركة Bubbly
+Animate.idle.Animation1.AnimationId = "http://www.roblox.com/asset/?id=910004836"
+Animate.idle.Animation2.AnimationId = "http://www.roblox.com/asset/?id=910009958"
+Animate.walk.WalkAnim.AnimationId = "http://www.roblox.com/asset/?id=910034870"
+Animate.run.RunAnim.AnimationId = "http://www.roblox.com/asset/?id=910025107"
+Animate.jump.JumpAnim.AnimationId = "http://www.roblox.com/asset/?id=910016857"
+Animate.fall.FallAnim.AnimationId = "http://www.roblox.com/asset/?id=910001910"
+Animate.swimidle.SwimIdle.AnimationId = "http://www.roblox.com/asset/?id=910030921"
+Animate.swim.Swim.AnimationId = "http://www.roblox.com/asset/?id=910028158"
+
+-- تحديث الحركة فوراً
+game.Players.LocalPlayer.Character.Humanoid.Jump = true
+    end,
+})
